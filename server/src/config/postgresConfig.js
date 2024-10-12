e553c5f61eee8b62c9a03fd8e72a8b8312b9ccf6
alex <sytnikov.zp@gmail.com>
2024-10-12 11:42:22 +0300

    Sequelize init on server

---
+++ server/src/config/postgresConfig.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.HOST,
+    dialect: process.env.DB_DIALECT,
+    migrationStorage: 'json',
+    seederStorage: 'json',
+  },
+  test: {},
+  production: {},
+};
