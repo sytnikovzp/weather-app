6b0cccb4587f3cf8864dc2158648d005c10f68e9
alex <sytnikov.zp@gmail.com>
2024-10-13 12:00:27 +0300

    Server: Added db seeders (users)

---
+++ server/src/db/seeders/a-users.js
@@ -0,0 +1,28 @@
+/* eslint-disable camelcase */
+const bcrypt = require('bcrypt');
+
+const { SALT_ROUNDS } = require('../../constants');
+
+const users = [
+  {
+    full_name: 'Test User',
+    email: 'test.user@gmail.com',
+    password: 'Qwerty12',
+    created_at: new Date(),
+    updated_at: new Date(),
+  },
+];
+
+module.exports = {
+  async up(queryInterface) {
+    for (const user of users) {
+      user.password = await bcrypt.hash(user.password, SALT_ROUNDS);
+    }
+
+    await queryInterface.bulkInsert('users', users, {});
+  },
+
+  async down(queryInterface) {
+    await queryInterface.bulkDelete('users', null, {});
+  },
+};
