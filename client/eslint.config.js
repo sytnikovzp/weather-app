d009fc592780147de35b885f3430fd26181d9828
alex <sytnikov.zp@gmail.com>
2024-10-13 05:31:07 +0300

    Created package 'client'

---
+++ client/eslint.config.js
@@ -0,0 +1,55 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+
+export default [
+  {
+    files: ['**/*.{js,jsx}'],
+    ignores: ['dist'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    settings: { react: { version: '18.3' } },
+    plugins: {
+      react,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+      'react/jsx-filename-extension': [
+        'warn',
+        {
+          extensions: ['.js', '.jsx'],
+        },
+      ],
+      'no-underscore-dangle': [
+        'error',
+        {
+          allow: ['_id'],
+        },
+      ],
+      camelcase: ['warn', { properties: 'always' }],
+      'react/jsx-no-target-blank': 'off',
+      'react/no-unescaped-entities': 'off',
+      'react/prop-types': 'off',
+      'no-undef': 'warn',
+      'no-unused-vars': 'warn',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  },
+];
