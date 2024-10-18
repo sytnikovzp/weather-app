454db44d1060dce1504d18891eacd1eb24d20f20
alex <sytnikov.zp@gmail.com>
2024-10-18 14:40:26 +0300

    Fixed Wind direction label

--- client/src/services/weatherService.js
+++ client/src/services/weatherService.js
@@ -9,19 +9,19 @@ export function getWindDirection(deg) {
   if ((deg >= 0 && deg <= 22.5) || (deg > 337.5 && deg <= 360)) {
     return 'Півн.';
   } else if (deg > 22.5 && deg <= 67.5) {
-    return 'Пн-Схід.';
+    return 'Пн-Сх.';
   } else if (deg > 67.5 && deg <= 112.5) {
     return 'Схід.';
   } else if (deg > 112.5 && deg <= 157.5) {
-    return 'Пд-Схід';
+    return 'Пд-Сх';
   } else if (deg > 157.5 && deg <= 202.5) {
     return 'Півд.';
   } else if (deg > 202.5 && deg <= 247.5) {
-    return 'Пд-Зах.';
+    return 'Пд-Зх.';
   } else if (deg > 247.5 && deg <= 292.5) {
     return 'Зах.';
   } else if (deg > 292.5 && deg <= 337.5) {
-    return 'Пн-Зах.';
+    return 'Пн-Зх.';
   } else {
     return 'Invalid degree';
   }
