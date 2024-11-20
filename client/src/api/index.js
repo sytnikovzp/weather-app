899a08acb5cca5850ca233cb7e847c0906f18d92
Sytnikov Aleksandr <sytnikov.zp@gmail.com>
2024-11-20 02:29:38 +0200

    Fixed 'fetchLocationByIP'

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -111,11 +111,18 @@ export const removeCityFromFavorites = async (latitude, longitude) => {
 
 export const fetchLocationByIP = async () => {
   try {
-    const response = await axios.get('https://ipapi.co/json');
+    let response = await axios.get('https://ipapi.co/json');
     const { latitude, longitude } = response.data;
     return { latitude, longitude };
   } catch (error) {
-    return handleError(error, 'Failed to fetch location by IP');
+    console.log(error.message, ': Error with ipapi.co, try ipwho.is...');
+    try {
+      let response = await axios.get('http://ipwho.is');
+      const { latitude, longitude } = response.data;
+      return { latitude, longitude };
+    } catch (error) {
+      return handleError(error, 'Failed to fetch location by IP');
+    }
   }
 };
 
