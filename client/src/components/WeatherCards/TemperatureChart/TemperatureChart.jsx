83a38512d5f4954f85fdb0dd91708b6392c3b2cc
Sytnikov Aleksandr <sytnikov.zp@gmail.com>
2025-05-02 05:05:44 +0300

    Fixed error in TemperatureChart component

--- client/src/components/WeatherCards/TemperatureChart/TemperatureChart.jsx
+++ client/src/components/WeatherCards/TemperatureChart/TemperatureChart.jsx
@@ -1,11 +1,7 @@
 /* eslint-disable consistent-return */
 import { useEffect, useRef, useState } from 'react';
+import Chart from 'chart.js/auto';
 
-import {
-  createChartInstance,
-  destroyChartInstance,
-  updateChartInstance,
-} from '../../../utils/chartHelpers';
 import useWeatherForCity from '../../../hooks/useWeatherForCity';
 
 import ErrorMessageBlock from '../../ErrorMessageBlock/ErrorMessageBlock';
@@ -40,14 +36,71 @@ function TemperatureChart({
     }
     const ctx = chartRef.current.getContext('2d');
     if (chartInstanceRef.current) {
-      updateChartInstance(chartInstanceRef.current, forecastData);
-    } else {
-      chartInstanceRef.current = createChartInstance(ctx, forecastData);
+      chartInstanceRef.current.data = {
+        ...forecastData,
+        datasets: forecastData.datasets.map((dataset) => ({
+          ...dataset,
+          backgroundColor: '#5B92D9',
+          borderColor: '#3A6EA5',
+          pointBackgroundColor: '#3A6EA5',
+          pointBorderColor: '#FFFFFF',
+          pointRadius: 4,
+          pointHoverRadius: 6,
+          tension: 0.3,
+        })),
+      };
+      chartInstanceRef.current.update();
+      return;
     }
+
+    chartInstanceRef.current = new Chart(ctx, {
+      type: 'line',
+      data: {
+        ...forecastData,
+        datasets: forecastData.datasets.map((dataset) => ({
+          ...dataset,
+          backgroundColor: '#5B92D9',
+          borderColor: '#3A6EA5',
+          pointBackgroundColor: '#3A6EA5',
+          pointBorderColor: '#FFFFFF',
+          pointRadius: 4,
+          pointHoverRadius: 6,
+          tension: 0.3,
+        })),
+      },
+      options: {
+        responsive: false,
+        maintainAspectRatio: false,
+        plugins: {
+          legend: { display: true },
+          tooltip: {
+            backgroundColor: '#FFFFFF',
+            borderColor: '#AAD4F5',
+            borderWidth: 1,
+            titleColor: '#333333',
+            bodyColor: '#000000',
+            padding: 10,
+            titleFont: { size: 14, weight: 'bold' },
+            bodyFont: { size: 12 },
+          },
+        },
+        interaction: { intersect: false },
+        scales: {
+          x: {
+            ticks: { color: '#333333' },
+            grid: { color: '#E0E0E0' },
+          },
+          y: {
+            ticks: { color: '#333333' },
+            grid: { color: '#E0E0E0' },
+          },
+        },
+      },
+    });
+
     return () => {
-      if (chartInstanceRef.current) {
-        destroyChartInstance(chartInstanceRef.current);
-      }
+      chartInstanceRef.current?.destroy();
+      chartInstanceRef.current = null;
     };
   }, [forecastData]);
 
